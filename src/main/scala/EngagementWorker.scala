815a264395601b7c59e7a747ccf7e10b954e550d
DanielaMazur <danielamazur2000@gmail.com>
2022-04-17 13:31:08 +0300

    Add engagement ratio computation

--- src/main/scala/EngagementWorker.scala
+++ src/main/scala/EngagementWorker.scala
@@ -9,6 +9,11 @@ class EngagementWorker extends Worker {
     case event: JsValue => {
       try {
         log.info((event \ "message" \ "tweet" \ "text").as[String])
+        val favorites = (event \ "message" \ "tweet" \ "favorite_count").as[Int]
+        val retweets = (event \ "message" \ "tweet" \ "retweet_count").as[Int]
+        val followers = (event \ "message" \ "tweet" \ "user" \ "followers_count").as[Int]
+        val engagementRatio = (favorites + retweets) / followers
+        log.info("Engagement ratio " + engagementRatio)
         Thread.sleep(Random.between(50, 500))
       }catch{
         case _: Throwable => ???
